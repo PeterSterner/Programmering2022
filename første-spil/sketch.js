95893cafbeb64a1f23e36fdb2c0192ee4c76a90b
Peter Sterner <psterner@gmail.com>
2022-09-22 05:57:49 +0200

    Update sketch.js

--- første-spil/sketch.js
+++ første-spil/sketch.js
@@ -1,16 +1,12 @@
-let x, y, d, a, b;
-
+let x = 50,
+  y = 50,
+  d = 40,
+  a = 5,
+  b = 0;
 let x1, y1, d1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
-  x = 50;
-  y = 50;
-  d = 40;
-  a = 5;
-  b = 0;
-
   x1 = windowWidth - 50;
   y1 = windowHeight - 50;
   d1 = 40;
@@ -25,32 +21,40 @@ function draw() {
 
   x = x + a;
   y = y + b;
+
+  let resultat = beregnAfstand();
+
+  if (resultat < d / 2 + d1 / 2 - 30) {
+    textSize(30);
+    textAlign(CENTER, CENTER);
+    text("Du har vundet!", windowWidth / 2, windowHeight / 2);
+    noLoop();
+  }
+}
+
+function beregnAfstand() {
+  return sqrt(pow(x - x1, 2) + pow(y - y1, 2));
 }
 
 function keyPressed() {
+  if (keyCode === DOWN_ARROW) {
+    a = 0;
+    b = 5;
+  }
+  if (keyCode === UP_ARROW) {
+    a = 0;
+    b = -5;
+  }
   if (keyCode === LEFT_ARROW) {
     a = -5;
     b = 0;
   }
-
   if (keyCode === RIGHT_ARROW) {
     a = 5;
     b = 0;
   }
-
-  if (keyCode === UP_ARROW) {
-    b = -5;
-    a = 0;
-  }
-
-  if (keyCode === DOWN_ARROW) {
-    b = 5;
-    a = 0;
-  }
-
   if (keyCode === 83) {
-    // vi har trykket på s
-    b = 0;
     a = 0;
+    b = 0;
   }
 }
